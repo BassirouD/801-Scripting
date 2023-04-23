3a9ff6e83a6d343ce2dfc1fca8f04cbdff714142
BassirouD <moubass.diallo@gmail.com>
2023-04-23 12:57:08 +0000

    Bash: Cours) Variables simples

---
+++ cours/variables.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+var1="Bonjour les amis"
+echo "$var1"
+
+listFic=$(ls)
+echo "$listFic"
+
+echo "Vous êtes : $USER"
+
+echo "Repertoire dexécution: $PWD"
+
+echo "Répertoire de base: : $HOME"
+
+exit 0
