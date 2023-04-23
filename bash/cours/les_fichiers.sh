ede8b776b0bd4d1938c6974ee4371bbd22e5b3e5
BassirouD <moubass.diallo@gmail.com>
2023-04-23 18:07:51 +0000

    Bash: Cours) Fichier: Manip file

--- bash/cours/les_fichiers.sh
+++ bash/cours/les_fichiers.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
-#Rédirection dans un fichier en écrivant dedans
+#1) Rédirection dans un fichier en écrivant dedans
 list=$(ls)
 for element in $list; do
     echo "$element" >> toto.txt
 done
 
 
+#2) Lecture d'un fichier
+    #- La redirection
+list=$(<toto.txt)
+for element in $list; do
+    echo "$element" >> toto2.txt
+done
 
+    #- Le cat
+list=$(cat toto.txt)
+for element in $list; do
+    echo "$element" >> toto3.txt
+done
+
+#3) Lecture ligne a ligne
+cpt=1
+while read LINE; do
+    echo $cpt ":" "$LINE"
+    let cpt++
+done < toto3.txt
