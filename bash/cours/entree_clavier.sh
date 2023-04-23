845420cab6bf7a787d187414070620d6ff4be6a9
BassirouD <moubass.diallo@gmail.com>
2023-04-23 17:07:52 +0000

    Bash: Cours) Lecture entrée clavier

---
+++ bash/cours/entree_clavier.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo Enrez votre nom:
+read -r v1
+
+echo Enrez votre prenom:
+read -r v2
+
+echo Enrez votre age:
+read -r v3
+
+echo "Vous avez entré $v1"
+echo "Vous avez entré $v2"
+echo "Vous avez entré $v3"
