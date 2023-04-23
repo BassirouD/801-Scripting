e9015144a2580228d0e59bcadff4dce098f74057
BassirouD <moubass.diallo@gmail.com>
2023-04-23 21:46:13 +0000

    Bash: Cours) Ajout et modification d'un delai variable sur une interface

--- bash/cours/commande_tc.sh
+++ bash/cours/commande_tc.sh
@@ -6,3 +6,9 @@ sudo tc qdisc add dev ens33 root netem delay 100ms
 
 #Suppression du delai
 sudo tc qdisc del dev ens33 root
+
+#Ajout d'un delai variable a une interface
+sudo tc qdisc add dev ens33 root netem delay 100ms 10ms
+
+
+sudo tc qdisc change dev ens33 root netem delay 200ms 200ms
