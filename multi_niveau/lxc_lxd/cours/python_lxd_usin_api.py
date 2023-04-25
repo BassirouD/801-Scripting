844839194b1d2fe87c0765cbefb9ae37cc7c8cf7
BassirouD <moubass.diallo@gmail.com>
2023-04-25 00:24:34 +0000

    Quelques methodes de l'api REST lxd

--- multi_niveau/lxc_lxd/cours/python_lxd_usin_api.py
+++ multi_niveau/lxc_lxd/cours/python_lxd_usin_api.py
@@ -16,3 +16,61 @@ print(r.status_code)
 print(r.text)
 print(r.json()['metadata']['expanded_devices']['eth0']['parent'])
 
+r = requests.delete('https://192.168.189.129:8443/1.0/containers/oli2',
+                    cert=('lxd.crt', 'lxd.key'), verify=False)
+
+print(r.status_code)
+
+payload = {
+    "name": "oli3"
+}
+
+mydata = json.dumps(payload).encode('utf-8')
+r = requests.post('https://192.168.189.129:8443/1.0/containers/oli2',
+                  cert=('lxd.crt', 'lxd.key'), verify=False, data=mydata)
+
+print(r.status_code)
+print(r.text)
+
+payload = {
+    "name": "oli4",
+    "source": {
+        "type": "image",
+        "certificate": "",
+        "figerprint": "9e7158fc0683"
+    },
+    "instance_type": ""
+}
+
+mydata = json.dumps(payload).encode('utf-8')
+r = requests.post('https://192.168.189.129:8443/1.0/containers',
+                  cert=('lxd.crt', 'lxd.key'), verify=False, data=mydata)
+
+print(r.status_code)
+print(r.text)
+
+payload = {
+    "action": "start"
+}
+
+mydata = json.dumps(payload).encode('utf-8')
+
+r = requests.put('https://192.168.189.129:8443/1.0/containers/oli2/state',
+                 cert=('lxd.crt', 'lxd.key'), verify=False, data=mydata)
+
+print(r.status_code)
+print(r.text)
+
+
+payload = {
+    "command": ["touch", "/tmp/toto.txt"],
+    "width": 80,
+    "height": 20
+}
+
+mydata = json.dumps(payload).encode('utf-8')
+r = requests.post('https://192.168.189.129:8443/1.0/containers/oli2/exec',
+                  cert=('lxd.crt', 'lxd.key'), verify=False, data=mydata)
+
+print(r.status_code)
+print(r.text)
