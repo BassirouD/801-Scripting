be793ac0f733ed6d151f2cfeb9c57254ba723f21
BassirouD <moubass.diallo@gmail.com>
2023-06-19 06:46:10 +0000

    SC sauvegarde avec python

---
+++ python/tps/sc-sauvegarde.py
@@ -0,0 +1,19 @@
+#!/usr/local/bin/python3
+import pysftp
+import sys
+import subprocess
+
+host = sys.argv[1]
+username = sys.argv[2]
+password = sys.argv[3]
+archive_name = sys.argv[4]
+folder_to_save_name = sys.argv[5]
+
+if len(sys.argv) == 5:
+    print('Tous les paramètres sont présent...')
+else:
+    print('Paramètres manquants')
+    exit(1)
+
+archive = subprocess.run(['tar', '-cvf', archive_name, folder_to_save_name])
+print(archive)
